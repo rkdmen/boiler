e33ca5384a93804709d88600543182f0c7f29d65
John KM Jung <min85da@gmail.com>
2017-01-13 00:27:34 -0500

    added redux to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,34 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
+import promise from 'redux-promise';
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+import App from './App.jsx';
+import rootReducer from './reducers/index';
+
+
+const Store = createStore(
+  combineReducers({
+    reducer: rootReducer,
+    routing: routerReducer
+  }),
+    applyMiddleware(promise)
+);
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, Store)
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <Router history={history}>
+          <Route path="/" component={App}>
+
+          </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('react-root')
+);
