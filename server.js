72a29d820aad4f215a7f2e281a4c228a5eaeef00
John KM Jung <min85da@gmail.com>
2017-01-13 00:25:46 -0500

    added process.env

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath
   })
-  .listen(3000, '0.0.0.0', function (err, result) {
+  .listen(process.env.PORT || 3000, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
     }
-
     console.log('Running at http://0.0.0.0:3000');
   });
