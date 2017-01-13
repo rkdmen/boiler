b661739c7bb4a99097018d1430d50706a6fd5cae
John KM Jung <min85da@gmail.com>
2017-01-13 00:31:50 -0500

    Main, Movie component added

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Col, Row, Grid } from 'react-bootstrap';
+import Movie from './Movie';
+
+class Main extends React.Component {
+  render(){
+    return (
+          <Grid className="main-container">
+            <Row>
+
+              <Col xs={12} md={12}>
+                  <Movie />
+              </Col>
+
+            </Row>
+          </Grid>
+      )
+  }
+}
+
+
+export default Main;
---
+++ src/components/Movie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NowPlayingContainer from '../containers/NowPlayingContainer'
+
+class Movie extends React.Component {
+  render(){
+    return (
+    <div>
+      <NowPlayingContainer />
+    </div>
+    )
+  }
+}
+
+export default Movie;
