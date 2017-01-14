5a08d0ff843af61b48fb49db0ee2d36cb94032e9
John KM Jung <min85da@gmail.com>
2017-01-13 21:35:06 -0500

    heroku

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,20 +1,28 @@
-var config = require('./webpack.config.js');
+var path = require('path');
 var webpack = require('webpack');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  })
-);
-
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  })
-);
-
-module.exports = config;
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  }
+}
